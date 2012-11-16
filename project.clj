119e191810fc4544470a58ce074bb27f95adf746
Janne Vaittinen <janne.vaittinen@iki.fi>
2012-11-16 11:44:12 +0200

    WTPFL

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject cljs-hello "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "WTFPL"
+            :url "http://sam.zoy.org/wtfpl/"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [noir "1.3.0-beta10"]]
   :plugins [[lein-cljsbuild "0.2.9"]]
