b0508148ea0503debaddc17fc2d08af11803060e
jvmakine <juho.makinen@solita.fi>
2012-11-16 17:19:21 +0200

    Tynnyrin lennätystä

--- src/cljs/warmit/world.cljs
+++ src/cljs/warmit/world.cljs
@@ -4,20 +4,37 @@
   {:catapult {:speed-x 0, :x 2000 
               :speed-y 0, :y 100 
               :speed-force 0, :force 0}
-   :barrel {:fire-time 0
-            :fire-x 0
-            :landing-time 0
+   :barrel {:x 0
+            :y 0
+            :z 0
+            :dz 0
             :launched? false
             }}
   )
 
-(defn fire [world x force t] 
-  (-> world 
-    (assoc-in [:barrel :fire-x] (-> world :catapult :x))
-    (assoc-in [:barrel :fire-time] t)
+(defn ms-to-s [ms] (* ms 0.001))
+
+(defn next-barrel-position [barrel dt] barrel
+  #_(-> barrel
+     (update-in [:y] #(+ % (* 2000 (ms-to-s dt))))
+     (update-in [:z] #(+ % (* 1000 (ms-to-s (-> barrel :dz)))))
+     (update-in [:dz] #(- % (* 500 (ms-to-s dt))))))
+
+(defn update-barrel [world dt]
+  (if (-> world :barrel :launched?) 
+    (assoc-in world [:barrel] (next-barrel-position (-> world :barrel) dt))
+    world))
+
+
+(defn fire [world x force t]
+  (-> world
+    (assoc-in [:barrel :x] (-> world :catapult :x))
+    (assoc-in [:barrel :y] (-> world :catapult :y))
+    (assoc-in [:barrel :z] 0)
+    (assoc-in [:barrel :dz] 10)
     (assoc-in [:barrel :launched?] true)))
 
-(defn update-firing [world t] 
+(defn update-firing [world t]
   (if (and (= (-> world :catapult :speed-force) 0)
            (not= (-> world :catapult :force) 0))
     (do 
@@ -37,9 +54,9 @@
 (defmethod update :space [world [_ value & _]]
   (assoc-in world [:catapult :speed-force] (if (= value :pressed) 100 0)))
 
-(defn ms-to-s [ms] (* ms 0.001))
-
 (defmethod update :dt  [world [_ value t]]
   (-> (update-firing world t)
+    (update-barrel value)
     (update-in [:catapult :x] (partial + (* (ms-to-s value) (-> world :catapult :speed-x))))
-    (update-in [:catapult :force] (partial + (* (ms-to-s value) (-> world :catapult :speed-force))))))
+    (update-in [:catapult :force] (partial + (* (ms-to-s value) (-> world :catapult :speed-force))))
+    ))
